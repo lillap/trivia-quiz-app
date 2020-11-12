2c69ca8d14f788ab66fbd745046032eda6e94afb
NORDIC\pkarim <paria.karim@gmail.com>
2020-11-12 09:09:43 +0100

    lillap feat: completed setup of homepage

---
+++ trivia-game-app/src/components/HomePage.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="homePage">
+    <h1>{{ msg }}</h1>
+    <p>
+      Get started
+      <button>Click here!</button>
+    </p>
+  
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
