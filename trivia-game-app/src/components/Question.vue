6d9cbf4f385072fc2d7719dd1e128484c7904c62
Oscar Dahlquist <43441099+Vattenkruka@users.noreply.github.com>
2020-11-13 09:31:28 +0100

    lillap & Vattenkruka Feat: nextQuestion functionality added to button

--- trivia-game-app/src/components/Question.vue
+++ trivia-game-app/src/components/Question.vue
@@ -2,10 +2,18 @@
   <div class="question-container">
     <h3>{{ currentQuestion.question }}</h3>
     <div class="answer-options">
-      <button id="redBtn">{{ answers[0].option }}</button>
-      <button id="yellowBtn">{{ answers[1].option }}</button>
-      <button id="greenBtn">{{ answers[2].option }}</button>
-      <button id="blueBtn">{{ answers[3].option }}</button>
+      <button id="redBtn" v-on:click="nextQuestion">
+        {{ answers[0].option }}
+      </button>
+      <button v-on:click="nextQuestion" id="yellowBtn">
+        {{ answers[1].option }}
+      </button>
+      <button v-on:click="nextQuestion" id="greenBtn">
+        {{ answers[2].option }}
+      </button>
+      <button v-on:click="nextQuestion" id="blueBtn">
+        {{ answers[3].option }}
+      </button>
     </div>
   </div>
 </template>
@@ -45,75 +53,76 @@ export default {
       }
       return shuffledAnswers;
     },
+    nextQuestion() {
+      this.$emit("next-question");
+    },
   },
 };
 </script>
 
 <style>
 .answer-options {
-  display:flex;
+  display: flex;
   flex-flow: row wrap;
 }
 
-  button {
-    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-    font-size: 1.5vw;
-    text-align: center; 
-    border-radius: 5px;
-    cursor: pointer;
-    margin: 2%;
-    padding-bottom: 2%;
-    padding-top: 2%;
-    height: 6rem;
-    width: 20%;
-    transition-duration: 0.4s;
-    box-shadow: 4px 4px rgb(164, 164, 172);
-    flex: 1 0 30%;
+button {
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+  font-size: 1.5vw;
+  text-align: center;
+  border-radius: 5px;
+  cursor: pointer;
+  margin: 2%;
+  padding-bottom: 2%;
+  padding-top: 2%;
+  height: 6rem;
+  width: 20%;
+  transition-duration: 0.4s;
+  box-shadow: 4px 4px rgb(164, 164, 172);
+  flex: 1 0 30%;
+}
 
-  }
+#redBtn {
+  background-color: red;
+  border: red;
+}
 
-  #redBtn {
-    background-color:red;
-    border: red;
-  }
+#redBtn:hover {
+  border: 3px solid red;
+  background-color: #ffffff;
+  box-shadow: 4px 4px rgb(73, 73, 77);
+}
 
-  #redBtn:hover {
-    border: 3px solid red;
-    background-color: #ffffff;
-    box-shadow: 4px 4px rgb(73, 73, 77);
-  }
+#yellowBtn {
+  background-color: yellow;
+  border: yellow;
+}
 
-  #yellowBtn {
-    background-color: yellow;
-    border: yellow;
-  }
+#yellowBtn:hover {
+  border: 3px solid yellow;
+  background-color: #ffffff;
+  box-shadow: 4px 4px rgb(73, 73, 77);
+}
 
-  #yellowBtn:hover {
-    border: 3px solid yellow;
-    background-color:#ffffff;
-    box-shadow: 4px 4px rgb(73, 73, 77);
-  }
+#greenBtn {
+  background-color: green;
+  border: green;
+}
 
-  #greenBtn {
-    background-color: green;
-    border: green;
-  }
+#greenBtn:hover {
+  border: 3px solid green;
+  background-color: #ffffff;
+  box-shadow: 4px 4px rgb(73, 73, 77);
+}
 
-  #greenBtn:hover {
-    border: 3px solid green;
-    background-color:#ffffff;
-    box-shadow: 4px 4px rgb(73, 73, 77);
-  }
+#blueBtn {
+  background-color: blueviolet;
+  border: blueviolet;
+}
 
-  #blueBtn {
-    background-color: blueviolet;
-    border: blueviolet;
-  }
-
-  #blueBtn:hover {
-    border: 3px solid blueviolet;
-    background-color:#ffffff;
-    box-shadow: 4px 4px rgb(73, 73, 77);
-  }
-  
+#blueBtn:hover {
+  border: 3px solid blueviolet;
+  background-color: #ffffff;
+  box-shadow: 4px 4px rgb(73, 73, 77);
+}
 </style>
